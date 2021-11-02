8c98bcd05c4af8491121da91748139a68f554b19
Hubert MUGABO <mugberto@gmail.com>
2021-11-02 17:23:28 +0200

    Add route for creating comment endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   end
   namespace :api do
     resources :posts, only: [:index] do 
-      resources :comments, only: [:index]
+      resources :comments, only: [:index, :create]
     end
 
   end
