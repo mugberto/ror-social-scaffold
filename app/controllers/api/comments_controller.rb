8ac932642a298831375cab110ac3a395b7a935ca
Hubert MUGABO <mugberto@gmail.com>
2021-11-03 10:49:28 +0200

    Add endpoint to post comment

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -3,4 +3,14 @@ class Api::CommentsController < Api::ApiController
     post = Post.find(params[:post_id])
     render json: post.comments
   end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(user_id: current_user.id, content: params[:content])
+    if @comment.save
+      render plain: 'Created', status: :created
+    else 
+      render plain: 'Not created', status: :no_content
+    end
+  end
 end
